195355280f89669edb6616bca5f6f63e85f1155f
devilcrowley <devilcrowley@github.com>
2018-06-17 03:23:48 +0300

    Проверка строки на палиндром

---
+++ pal.js
@@ -0,0 +1,25 @@
+function palindrome(s) {
+    var res = true;
+    s = s.toString().toLowerCase().replace(/\s/g,'');
+    s = s.replace(/[^a-zA-Zа-яА-Я]/g, '');
+        
+    for(var i=0, j=parseInt(s.length/2), k=s.length-1; i<j; i++, k--) {
+        if(s.charAt(i) != s.charAt(k)) {
+            res = false;
+            break;
+        }        
+    }
+    
+    return res;
+}
+
+var valuepal = 0;
+valuepal = process.argv[2];
+
+if (palindrome(valuepal) === true) {
+	console.log("YES");
+} else {
+	console.log("NO");
+}
+
+// coded for GeekBrains
